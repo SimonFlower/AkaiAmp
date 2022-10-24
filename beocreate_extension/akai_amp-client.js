64737399356db4a0d15a2c46b745ff95a741f2a0
Simon Flower <smf99uk@gmail.com>
2022-10-25 00:11:35 +0100

    WIP: building application

--- beocreate_extension/akai_amp-client.js
+++ beocreate_extension/akai_amp-client.js
@@ -1,13 +1,83 @@
 /* Client side code for the akai_amp extension 
  *
- * Client side functions (power, volume, reset, request_status) call
- * a handler on the server side. The server side then calls exec to
- * run akai_amp.py (with approriate arguments) to send requests to
- * the relay card. The stdout and stderr from the exec call are
- * returned to the client once the call has completed on the "akai_amp"
- * handler in this script */
+ * Client side functions (toggle_power, set_volume, reset, 
+ * get_power_status) call a handler on the server side to
+ * request actions and the retrieve information about the
+ * state of the amplifier.
+ *
+ * A message is sent from the client with the following fields
+ * (this corresponds to the beocreate2 standard for client/server
+ * messaging):
+ *
+ *   target: "akai_amp"
+ *   header: "action" | "status"
+ *   content.client_id: a unique ID for the client
+ *   content.* other fields in content are dependent on whether an 
+ *             "action" or a "status" message is being sent
+ *
+ * For "action" messages the content is as follows:
+ *
+ *   content.command: The command verb for the akai_amp.py script
+ *   content.params: (optional) any additional parameters for the script
+ *
+ * For "status" messages the content is as follows:
+ *
+ *   content.request: "power" - request the status of the power relay
+ *
+ * The server responds differently to "action" and "status" messages,
+ " since "action" messages may require some time to complete. For
+ " "status" messages, the server responds immediately with the
+ * following response message:
+ *
+ *   header: "status"
+ *   content.client_id: contents of the requesting client's content.id field
+ *   content.orig_request: contents of the client's content.request message field
+ *   content.response: the servers response
+ *
+ * For "action" messages, provided it is possible for the server to fulfill
+ * the request, the server responds immediately with an acknowledgement message
+ * which looks like this:
+ *
+ *   header: "acknowledgement"
+ *   content.client_id: contents of the requesting client's content.id field
+ *   content.orig_command: contents of the client's content.command message field
+ *   content.orig_params: contents of the client's content.params message field
+ *   content.cmd: the command that was sent to the akai_amp.py script
+ *
+ * The server will always respond with a completion message (whether or not an
+ * acknowledgement has been sent). The completion message looks like this:
+ *
+ *   header: "completion"
+ *   content.client_id: contents of the requesting client's content.id field
+ *   content.orig_command: contents of the client's content.command message field
+ *   content.orig_params: contents of the client's content.params message field
+ *   content.cmd: the command that was sent to the akai_amp.py script
+ *   content.err_title: null OR a title for an error that occurred while calling the script
+ *   content.err_msg: null OR the contents of an error message that occurred while calling the script
+ */
 
-/* function to display an error message */
+// client ID must be unique for each client
+client_id = String(Date.now().toString(32) + Math.random().toString(16)).replace(/\./g, '');
+
+// function to set the state of the power on/off widget
+function set_power_on_widget (power_on) {
+    if (power_on)
+        document.getElementById ("akai_amp_power_toggle").classList.add ("on");
+    else
+        document.getElementById ("akai_amp_power_toggle").classList.remove ("on");
+}
+
+// function to update the status area
+function show_status (status_msg) {
+    if (status_msg) {
+        document.getElementById ("status_message_body").innerText = status_msg;
+        document.getElementById ("status_message").classList.remove ("hidden");
+    } else {
+        document.getElementById ("status_message").classList.add ("hidden");
+    }
+}
+
+// function to display an error message
 function show_error (err_title, err_msg) {
     if (err_title) {
         document.getElementById ("error_message_title").innerText = err_title;
@@ -18,55 +88,99 @@ function show_error (err_title, err_msg) {
     }
 }
 
+// function to create a status request message to send to server
+function make_status_msg (request) {
+    msg = {target: "akai_amp", header: "status", 
+           content: {client_id: client_id, 
+                     request: request}};
+    return msg;
+}
+
+// function to create an action request message to send to server
+function make_action_msg (command, params) {
+    msg = {target: "akai_amp", header: "action", 
+           content: {client_id: client_id, 
+                     command: command,
+                     params: params}};
+    return msg;
+}
+
 var akai_amp = (function() {
-        
-    /* this event handler receives messages from the server side code */
+    /* this event handler receives messages from the server for this extension */
     $(document).on("akai_amp", function(event, data) {
-        if (data.content.err_title) {
-            show_error (data.content.err_title, data.content.err_msg);
-        } else {
-            show_error (null, null);
-            switch (data.header) {
-                case "power":
-                    break;
-                case "volume":
-                    break;
-                case "reset":
-                    break;
-                case "request_status":
-                    if (data.content.stdout.substring (1,2) == "1") {
-                    } else if (data.content.stdout.substring (1,2) == "0") {
-                    } else {
-                        show_error ("Software Error", "Relay status unrecognised: " + data.content.stdout);
-                    }
-                    break;
-                default:
-                    show_error ("Software Error", "Unrecognised message received from akai_amp server code: " + data.header);
-                    break;
-           }
+        switch (data.header) {
+            case "status":
+                switch (data.content.orig_request) {
+                    case "power":
+                        // this is the response to a request for the status of the mains power relay
+                        set_power_on_widget (data.content.response);
+                        break;
+                    default:
+                        console.error (data);
+                        break;
+                }
+                break;
+            case "acknowledgement":
+                // only update the status where it was this client that sent the request
+                if (data.content.client_id == client_id)
+                    show_status ("Processing '" + data.content.orig_command + "' request");
+                break;
+            case "completion":
+                // clear the status message and set any error message
+                // (provided the request came from this client)
+                if (data.content.client_id == client_id) {
+                    show_status (null);
+                    if (data.content.err_title)
+                        show_error (data.content.err_title, data.content.err_msg);
+                    else
+                        show_error (null, null);
+                }
+                // if this was a power on/off command or a reset command
+                // then update the power toggle widget in the UI
+                if (! data.content.err_title) {
+                    if (data.content.orig_command === "power") {
+                        if (data.content.orig_params.startsWith ("on"))
+                            set_power_on_widget (true);
+                        else if (data.content.orig_params.startsWith ("off"))
+                            set_power_on_widget (false);
+                    } else if (data.content.orig_command === "reset")
+                        set_power_on_widget (false);
+                }
+                break;
+            default:
+                console.error (data);
+                break;
+        }
+    });
+    
+    /* this event handler receives messages on the "general" channel */
+    $(document).on("general", function(event, data) {
+        if (data.header === "activatedExtension" && data.content.extension == "akai_amp") {
+            // this extension has been started - initialise it by requesting the status of
+            // the mains power relay
+            beo.send(make_status_msg ("power"));
         }
     });
         
-    /* function to set the power state of the amplifier */
-    function power(on) {
-        beo.ask();
-        beo.send({target: "akai_amp", header: "power", content: {on: on}});
-    }
-        
     /* function to set the volume of the amplifier */
-    function volume(up, amount) {
-        beo.send({target: "akai_amp", header: "volume", content: {up: up, amount: amount}});
+    function set_volume(up, amount) {
+        beo.send(make_action_msg ("volume", up + " " + amount));
     }
         
     /* function to send a reset "all" request to the amplifier */
     function reset() {
-        beo.send({target: "akai_amp", header: "reset"});
+        beo.send(make_action_msg ("reset", "all"));
     }
-
-    /* function to request status of relays - the response will be returned
-     * via the event handler above */
-    function request_status() {
-        beo.send({target: "akai_amp", header: "request_status"});
+    
+    /* function to change state of power, dependent on current
+     * state of power on/off widget in UI - the state of the power on/off
+     * widget is changed when the response to the message is received */
+    function toggle_power() {
+        if (document.getElementById ("akai_amp_power_toggle").classList.contains ("on")) {
+            beo.send(make_action_msg ("power", "off"));
+        } else {
+            beo.send(make_action_msg ("power", "on"));
+        }
     }
 
     /* the returned structure maps functions in the "akai_amp" object
@@ -74,14 +188,21 @@ var akai_amp = (function() {
      * call the power method:
      *   onclick="akai_amp.power('on')" */
     return {
-        power: power,
-        volume: volume,
+        set_volume: set_volume,
         reset: reset,
-        request_status: request_status
+        toggle_power: toggle_power
     };
 })();
 
 /* request the status of the relays when the page loads */
-document.addEventListener("load", function(){
-    akai_amp.request_status();
-});
+/*
+function akai_amp_init() {
+    beo.send(make_status_msg ("power"));
+}
+if (window.attachEvent) 
+    window.attachEvent('onload', akai_amp_init);
+else if (window.addEventListener) 
+    window.addEventListener('load', akai_amp_init, false);
+else
+    document.addEventListener('load', akai_amp_init, false);
+*/
--- beocreate_extension/index.js
+++ beocreate_extension/index.js
@@ -6,61 +6,120 @@
 var exec = require('child_process').exec;
 var version = require("./package.json").version;
 
-/* code that is called when the system initialises the akai_amp extension */
-beo.bus.on('general', function(event) {
-    if (event.header == "activatedExtension") {
-        if (event.content.extension == "akai_amp") {
-            /* send the status of the relays to the client */
-            handle_client_call ({header: "request_status"});
-        }
-    }
+// global state for the server
+// this variable shows whether there is an active communication session with the relay card
+var relays_busy = false;
+// this variable is updated to hold the status of the mains power relay
+var power_on = false;
+
+// get the initial state of the power relay
+exec("akai_amp.py status short", function(error, stdout, stderr) {
+    console.log ("akai_amp: power request data: " + stdout);
+    if (stdout.length < 4)
+        console.error ("akai_amp: bad response from 'akai_amp.py status short': " + stdout);
+    else if (stdout.substring (0,1) == "1")
+        power_on = true;
+    else if (stdout.substring (0,1) == "0")
+        power_on = false;
+    else
+        console.error ("akai_amp: bad response from 'akai_amp.py status short': " + stdout);
+    console.log ("akai_amp: initial power relay state: " + String (power_on));
 });
 
-/* code that is called when the client side sends messages to the server side
- * provided the request is recognised, call the akai_amp.py script with
- * appropriate parameters, then return the stdout/stderr from the call to
- * the client */
-function handle_client_call (event) {
-    switch (event.header) {
-        case "power":
-            akai_amp_cmd = "power " + event.content.on;
-            break;
-        case "volume":
-            akai_amp_cmd = "volume " + event.content.up + " " + event.content.amount;
-            break;
-        case "reset":
-            akai_amp_cmd = "reset all";
-            break;
-        case "request_status":
-            akai_amp_cmd = "status short";
-            break;
-        default:
-            akai_amp_cmd = "";
-            console.error ("AkaiAmp: Unknown user request: " + event.header);
-            break;
-    }
-    if (akai_amp_cmd.length > 0) {
-        if (beo.debug) console.log ("AkaiAmp: User requested " + akai_amp_cmd);
-        exec("akai_amp.py " + akai_amp_cmd, function(error, stdout, stderr) {
-            if (error) {
-                err_title = "Error with amplifier";
-                if (stderr)
-                    err_msg = stderr;
-                else
-                    err_msg = "Unknown problem";
-                console.error (err_title + ": " + err_msg);
-            } else {
-                err_title = null;
-                err_msg = null;
-            }
-            beo.sendToUI("akai_amp", {header: event.header, 
-                                      content: {err_title: err_title, err_msg: err_msg, 
-                                                stdout: stdout, stderr: stderr}});
-        });
-    }
+
+function make_status_msg (client_msg, response) {
+    status_msg = {header: "status",
+                  content: {client_id: client_msg.content.client_id,
+                            orig_request: client_msg.content.request,
+                  response: response}};
+    return status_msg;
 }
-beo.bus.on('akai_amp', function(event) {
-    handle_client_call (event);
+
+function make_acknowledge_msg (client_msg) {
+    // construct the command for the akai_amp.py script
+    cmd = client_msg.content.command;
+    if (client_msg.content && client_msg.content.params)
+        cmd += " " + client_msg.content.params;
+    // construct the acknowledgement message
+    ack_msg = {header: "acknowledgement", 
+               content: {client_id: client_msg.content.client_id,
+                         orig_command: client_msg.content.command,
+                         orig_params: client_msg.content.params,
+                         cmd: cmd}};
+    return ack_msg;
+}
+
+function make_completion_msg (client_msg, error, stderr) {
+    if (error) {
+        err_title = "Error with amplifier";
+        if (stderr)
+            err_msg = stderr;
+        else
+            err_msg = "Unknown problem";
+        console.error (err_title + ": " + err_msg);
+    } else {
+        err_title = null;
+        err_msg = null;
+    }
+    comp_msg = {header: "completion", 
+                content: {client_id: client_msg.content.client_id,
+                          orig_command: client_msg.content.command,
+                          orig_params: client_msg.content.params,
+                          err_title: err_title, 
+                          err_msg: err_msg}};
+    return comp_msg;
+}
+
+/* Code that is called when the client side sends messages to the server side */
+beo.bus.on('akai_amp', function(client_msg) {
+    // what type of request is it?
+    switch (client_msg.header) {
+        case "status":
+            // what parameter is status required for?
+            switch (client_msg.content.request) {
+                case "power":
+                    response = make_status_msg (client_msg, power_on);
+                    beo.sendToUI("akai_amp", response);
+                    console.log (response);
+                    break;
+                default:
+                    console.error ("akai_amp: unrecognised action request: " + client_msg.content.request);
+                    break;
+            }
+            break;
+        case "action":
+            // are the relays busy?
+            if (relays_busy) {
+                error = "Amplifier busy processing another request.";
+                beo.sendToUI("akai_amp", make_completion_msg (client_msg, true, error));
+            } else {
+                // build the acknowledge message, which also contains the command to run
+                ack_msg = make_acknowledge_msg (client_msg);
+                beo.sendToUI("akai_amp", ack_msg);
+                // run the command
+                relays_busy = true;
+                exec("akai_amp.py " + ack_msg.content.cmd, function(error, stdout, stderr) {
+                    relays_busy = false;
+                    // check for a power on/off message and update the power status variable
+                    if (client_msg.content.command === "power") {
+                        if (client_msg.content.params.startsWith ("on"))
+                            power_on = true;
+                        else if (client_msg.content.params.startsWith ("off"))
+                            power_on = false;
+                    }
+                    // check for a reset message and update the power status variable
+                    // process the output from the script and send a completion message
+                    if (client_msg.content.command === "reset")
+                        power_on = false;
+                    // send the completion message to the client
+                    beo.sendToUI("akai_amp", make_completion_msg (client_msg, error, stderr));
+                });
+            }
+            break;            
+        default:
+            console.error ("akai_amp: unrecognised request type: " + client_msg.header);
+            break;
+    }
 });
 		
 module.exports = {
